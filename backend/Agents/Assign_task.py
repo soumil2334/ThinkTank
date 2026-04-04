d4281a4a5869f9055c572b44866bec2cce4a9c39
Soumil Sinha <soumil2433@gmail.com>
2026-04-04 20:41:04 +0530

    Changes made in Assign task

--- backend/Agents/Assign_task.py
+++ backend/Agents/Assign_task.py
@@ -5,6 +5,10 @@ from typing import List
 from langchain_openai import ChatOpenAI
 from langchain_core.messages import HumanMessage, AIMessage, SystemMessage, ToolMessage
 import requests
+from datetime import date
+import logging
+import time
+
 import os
 from dotenv import load_dotenv
 
@@ -12,7 +16,7 @@ load_dotenv()
 
 API_KEY = os.getenv('TRELLO_API')
 TOKEN = os.getenv('TRELLO_TOKEN')
-
+today = date.today()
 
 class checklist_item(BaseModel):
     name: str = Field(
@@ -48,9 +52,77 @@ class Boards(BaseModel):
     lists: List[Card_List] = Field(
         description="All workflow stages and their tasks")
     
+def get_boards():
+    url = "https://api.trello.com/1/members/me/boards"
+    
+    params = {
+        "key": API_KEY,
+        "token": TOKEN
+    }
+
+    return requests.get(url, params=params).json()
+
+def get_board_id_by_name(name):
+    boards = get_boards()
+    
+    for board in boards:
+        if board["name"] == name:
+            return board["id"]
+    
+    return None
+
+
+def get_board_members(board_id):
+    url = f"https://api.trello.com/1/boards/{board_id}/members"
+    
+    params = {
+        "key": API_KEY,
+        "token": TOKEN
+    }
+
+    response = requests.get(url, params=params)
+    return response.json()
+
+def Board_members_list(state: State):
+    start = time.time()
+
+    board_name = state['board_name']
+    board_id = get_board_id_by_name(name=board_name)
+
+    if not board_id:
+        raise Exception("Board not found")
+
+    member_details = get_board_members(board_id)
+    members = state['members_list']
+
+    trello_lookup = {}
+
+    for m in member_details:
+        full_name = m.get("username", "").lower()
+        first_name = full_name.split(" ")[0]
+
+        trello_lookup[first_name] = m.get("id")
+
+    updated_members = []
+    for member in members:
+        updated = {**member} 
+        name = updated.get('username', '').lower().split(" ")[0]
+        updated['member_trello_id'] = trello_lookup.get(name, "")
+        updated_members.append(updated)
+
+    end = time.time()
+    print(f'Assign members {end-start}')
+    return {
+        'board_id' : board_id,
+        'members_list': updated_members
+    }
+
 
 def Extract_tasks(state : State):
+    start=time.time()
     conversation_history = ''
+    
+    board_name=state['board_name']
 
     messages = state['messages']
     for message in messages:
@@ -67,17 +139,14 @@ def Extract_tasks(state : State):
     members_info=''
 
     for member in members:
-        members_info+=f""" Name : {member.get('member_name')}, 
+        members_info+=f""" Username : {member.get('username')}, 
                            Role : {member.get('member_role')},
-                           Member Trello_id : {member.get('member_trello')} \n"""
+                           Member Trello_id : {member.get('member_trello_id')} \n"""
 
-    system_instruction = f"""
+    system_instruction = """
 You are a project planning assistant.
 
 Your task is to analyze a conversation and convert it into a structured Trello board plan.
-
-If there was a previous generation of the Trello board plan and the user has rejected the result:
-Feedback : {state.get('Board_feedback', '')} For Output : {state.get('board_outline', )}
 
 You must:
 1. Extract the project goal
@@ -93,23 +162,35 @@ You must:
 You are given:
 
 1. Conversation history (brainstorming discussion)
-2. Team member details:
-   - Name
-   - Role
-   - Trello Member ID
+2. Board Name
+3. Information of the Team Members
 
 ---
 
 Assignment rules:
 - Match task requirements with member roles
-- Assign ONLY using the provided Trello member_id
-- If no clear match, leave member_id as an empty string ""
+- Assign ONLY the Trello member_id 
 
 ---
 
 Checklist rules:
 - Break each task into small, actionable steps
 - Each checklist item should be atomic (can be done in 1–2 hours)
+
+---
+
+Due Date Rules:
+- You MUST assign a due_date for every task
+- Use the provided today's date as reference
+- Estimate duration based on task complexity:
+  • Small tasks → 1–2 days
+  • Medium tasks → 3–5 days
+  • Large tasks → 5–10 days
+- Ensure due_date is AFTER today's date
+- Use strict Trello format: YYYY-MM-DDTHH:MM:SSZ
+- Always set time to 18:00:00Z
+- Do NOT leave due_date empty
+- Do NOT use relative terms (e.g., tomorrow, next week)
 
 ---
 
@@ -152,17 +233,25 @@ Schema to follow:
 
 """
     human_instruction=f"""
+Board Name : {board_name}
+
 Brainstorming discussion : {conversation_history}
 
 Team Information : {members_info}
+
+Today's date for reference : {today}
+    
+If there was a previous generation of the Trello board plan and the user has rejected the result:
+Feedback : {state.get('Board_feedback', '')} For Output : {state.get('board_outline', '')}
 """
+
     llm = ChatOpenAI(model='gpt-4o-mini')
     llm=llm.with_structured_output(Boards)
     response=llm.invoke([SystemMessage(content=system_instruction), HumanMessage(content=human_instruction)])
-    
+    end=time.time()
+    print(f'Extract tasks {end-start}')
     return {
-        'board_name' : response.Board_name,
-        'board_outline' : response
+        'board_outline' : response.model_dump()
     }
 
 def human_review(state : State):
@@ -171,38 +260,26 @@ def human_review(state : State):
         'Board_Name' : state['board_name'],
         'Board_Outline' : state['board_outline']
     })
-    changes = response.Board_Outline # if only minor changes
-    approval = response.status # if start agent again
-    feedback = response.feedback
+    changes = response.get('Board_Outline', '') # if only minor changes
+    approval = response.get('status', '') # if start agent again
+    feedback = response.get('feedback', '')
 
     if approval=='approved':
         return Command(
-            goto='Create Board',
+            goto='Trello',
             update={
-                'board_outline' : changes
+                'board_outline' : changes if changes else state['board_outline']
             }
         )
     
     if approval=='rejected':
         return Command(
-            goto='Extract Tasks',
+            goto='Extract_Tasks',
             update={
                 'Board_feedback' : feedback
             }
         )
 
-def create_board(name):
-    url = "https://api.trello.com/1/boards/"
-    
-    params = {
-        "name": name,
-        "defaultLists": "true",
-        "key": API_KEY,
-        "token": TOKEN
-    }
-
-    response = requests.post(url, params=params)
-    return response.json()
 
 def create_list(board_id, list_name):
     url = "https://api.trello.com/1/lists"
@@ -217,13 +294,13 @@ def create_list(board_id, list_name):
     response = requests.post(url, params=params)
     return response.json()
 
-def create_card(list_id, title, description=""):
+def create_card(list_id, title, description):
     url = "https://api.trello.com/1/cards"
     
     params = {
         "idList": list_id,
         "name": title,
-        "desc": description,   # 👈 add description here
+        "desc": description,  
         "key": API_KEY,
         "token": TOKEN
     }
@@ -284,67 +361,60 @@ def set_due_date(card_id, due_date):
 
 
 def Trello_agent(state:State):
+
+    print(f'Trello Agent started')
     board_outline=state['board_outline']
-    
-    if not board_outline.Board_name:
-        raise Exception("Board name missing")
 
-    board_name=board_outline.Board_name
+    board_name=state['board_name']
 
-    Trello_board=create_board(board_name)
+    Trello_board_id=state['board_id']
 
-    Trello_board_id=Trello_board.get('id')
-
-    Lists=board_outline.lists
+    Lists=board_outline.get('lists')
 
     for lst in Lists:
-        lst_name=lst.name
+        lst_name=lst.get('name')
 
         Trello_lst=create_list(board_id=Trello_board_id, list_name=lst_name)
 
         Trello_list_id=Trello_lst.get('id')
 
-        cards=lst.cards
+        cards=lst.get('cards')
 
         for card in cards:
-            card_name=card.name
-            card_desc=card.description
-            card_assignee_id=card.assignee
-            card_due_date=card.due_date
+            card_name=card.get('name')
+            card_desc=card.get('description')
+            card_assignee_id=card.get('assignee', None)
+            card_due_date=card.get('due_date', None)
 
 
-            Trello_card=create_card(Trello_list_id, card_name, description = card_desc)
+            Trello_card=create_card(Trello_list_id, card_name, card_desc)
 
             Trello_card_id=Trello_card.get('id')
-             
+            print(f'{board_outline}\n {lst} \n{card}')
+            
             if card_due_date:
                 set_due_date(Trello_card_id, card_due_date)
 
             if card_assignee_id:
                 assign_member(Trello_card_id, card_assignee_id)
 
-            checklists=card.checklists
+            checklists=card.get('checklists')
 
             for Checklist in checklists:
-                checklist_name=Checklist.name
+                checklist_name=Checklist.get('name')
 
                 Trello_checklist=create_checklist(Trello_card_id, checklist_name)
 
                 Trello_checklist_id=Trello_checklist.get('id')
 
-                Checklist_items=Checklist.items
+                Checklist_items=Checklist.get('items')
 
                 for item in Checklist_items:
-                    item_name=item.name
+                    item_name=item.get('name')
 
                     Trello_item = add_checklist_item(Trello_checklist_id, item_name)
 
     return {
-        'board_id' : Trello_board_id
+        'board_id' : Trello_board_id,
+        'messages': [AIMessage(content=f"Trello board '{board_name}' created successfully.")]
     }
-
-
-
-
-    
-    
